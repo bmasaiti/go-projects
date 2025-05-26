2fd69ed29270e5feecbb7363dfa6c4a0bf9a7c92
Tamuka <tamuka@budwell>
2025-05-26 23:07:46 +1000

    Fixed PR reviews comments

--- my-vault/internal/handlers/handlers.go
+++ my-vault/internal/handlers/handlers.go
@@ -4,15 +4,28 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/bmasaiti/go-projects/my-vault/internal/domain"
-	"github.com/bmasaiti/go-projects/my-vault/internal/storage"
 	"github.com/google/uuid"
 )
 
+//var db = storage.NewInMemorySecretRepo()
+//return domain.Secret{}, fmt.Errorf("secret with ID '%s' not found", secretId)
+//fmt.Errorf("no secrets found in the secrets store")
+var ErrNotFound = errors.New("not found")
+type SecretsRepository interface {
+	PutNewSecret(secret domain.Secret) error
+	GetScretsById(Id string) (domain.Secret, error)
+	DeleteSecretByID(Id string) (string, error)
+	ListAllSecrets() ([]domain.Secret, error)
+}
 
-var db = storage.NewInMemorySecretRepo()
+// Couldn't mock the tests , need to inject the db
+type SecretHandler struct {
+	DB SecretsRepository
+}
 
 type CreateSecretRequest struct {
 	Name  string            `json:"name"`
@@ -32,10 +45,10 @@ type GetSecretResponse struct {
 }
 
 type ListSecretsResponse struct {
-	Message string `json:"message"`
+	Message string          `json:"message"`
 	Secrets []domain.Secret `json:"Secrets"`
-
 }
+
 func GenerateUUID() string {
 	newUUID, err := uuid.NewV7()
 	if err != nil {
@@ -62,15 +75,14 @@ func NewCreateSecret(s CreateSecretRequest) domain.Secret {
 	}
 }
 
-func BuildListSecretsResponse(s []domain.Secret ) ListSecretsResponse{
+func BuildListSecretsResponse(s []domain.Secret) ListSecretsResponse {
 	return ListSecretsResponse{
 		Message: "Secrets fetched successfully",
 		Secrets: s,
 	}
 }
 
-
-func HandlePostSecret(w http.ResponseWriter, r *http.Request) {
+func (h *SecretHandler) HandlePostSecret(w http.ResponseWriter, r *http.Request) {
 
 	//var secret Secret
 	var secretRequestObject CreateSecretRequest
@@ -80,16 +92,19 @@ func HandlePostSecret(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Printf("ERROR: Unexpected behaviour: %v", err)
 		return
 	}
 	// build new secretObject
 	temp := NewCreateSecret(secretRequestObject)
-	err = db.PutNewSecret(temp)
+	//err = db.PutNewSecret(temp)
+	err = h.DB.PutNewSecret(temp)
 
 	if err != nil {
-		
+
 		err := errors.New("unexpected internal error")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("ERROR: Unexpted internal error: %v", err)
 		return
 	}
 
@@ -98,26 +113,31 @@ func HandlePostSecret(w http.ResponseWriter, r *http.Request) {
 		Name:    temp.Name,
 		Message: fmt.Sprintf("Successfully created secret with secretId: %s and name: %s", temp.Id, temp.Name),
 	}
-	fmt.Printf("Secret saved----------------------------------------- %s", temp.Id)
+	
+	log.Printf("INFO: Secret saved----------------------------------------- %s", temp.Id)
 	w.Header().Set("Content-Type", "application/json")
 	encoder := json.NewEncoder(w)
 	err = encoder.Encode(res)
 	if err != nil {
-		
+
 		err := errors.New("unexpected internal error")
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("ERROR: Unexpted internal error: %v", err)
 		return
 	}
 }
 
-func HandleGetSecretById(w http.ResponseWriter, r *http.Request) {
+func (h *SecretHandler) HandleGetSecretById(w http.ResponseWriter, r *http.Request) {
 	//curl  -X POST -H "Content-Type: application/json" http://localhost:9000/secrets/234
 	secretID := r.PathValue("secret_id")
+	secretEntry, err := h.DB.GetScretsById(secretID)
 	
-	secretEntry,err := db.GetScretsById(secretID)
-	
-	if err!=nil {
+	if err.Error() == ErrNotFound.Error(){
 		http.Error(w, fmt.Sprintf("Secret with ID %s not found", secretID), http.StatusNotFound)
+		return
+	} else if err != nil{
+		http.Error(w, "Unexpected internal error", http.StatusInternalServerError)
+		log.Printf("ERROR: Unexpted internal error: %v", err)
 		return
 	}
 
@@ -127,28 +147,31 @@ func HandleGetSecretById(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Retrieved secret----------------------------------------- %s", getSecretResponse)
 }
 
-
-func HandleDeleteSecretById(w http.ResponseWriter, r *http.Request) {
+func (h *SecretHandler) HandleDeleteSecretById(w http.ResponseWriter, r *http.Request) {
 	//curl  -X POST -H "Content-Type: application/json" http://localhost:9000/secrets/234
 	secretId := r.PathValue("secret_id")
-	secret,_ := db.DeleteSecretByID(secretId)
-
-	if secret != "" {
-		fmt.Println("Deleted secret with id -----------------------------------------", secret)
-		fmt.Fprintf(w, "Secret with ID %s deleted successfully", secret)
-
-	}else {
+	secret, err := h.DB.DeleteSecretByID(secretId)
+	if err.Error()==ErrNotFound.Error(){
 		http.Error(w, fmt.Sprintf("Secret with ID %s not found", secret), http.StatusNotFound)
 		return
 	}
-
+	if err != nil {
+		log.Printf("Failed to delete secret: %v", err)
+		return
+	}
+	log.Println("Deleted secret with id -----------------------------------------", secret)
+	fmt.Fprintf(w, "Secret with ID %s deleted successfully", secret)
 }
 
-func HandleListSecrets(w http.ResponseWriter, r *http.Request) {
+func (h *SecretHandler) HandleListSecrets(w http.ResponseWriter, r *http.Request) {
 
-	secrets, err := db.ListAllSecrets()
-	if err!=nil{
-		http.Error(w, "No secrets found in the secrets store", http.StatusNotFound)
+	secrets, err := h.DB.ListAllSecrets()
+	if err.Error() == ErrNotFound.Error(){
+		http.Error(w,"No secrets found in the vault", http.StatusNotFound)
+		return
+	} else if err != nil{
+		http.Error(w, "Unexpected internal server error", http.StatusInternalServerError)
+		log.Printf("ERROR: Unexpted internal error: %v", err)
 		return
 	}
 	response := BuildListSecretsResponse(secrets)
